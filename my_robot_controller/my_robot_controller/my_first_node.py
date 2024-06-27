cac7e5997b09dd609a9da1e9350fc44806f41707
Adwait Mane <mane.learn.2023.04@gmail.com>
2024-06-26 22:04:06 -0400

    Initial commit. All files.

---
+++ my_robot_controller/my_robot_controller/my_first_node.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class MyNode(Node): # MyNode inherits from Node in rclpy.node.
+
+    def __init__(self): # Create a constructor.
+        super().__init__("first_node")
+        # super() refers to the parent class i.e. the Node class in rclpy.node.
+        # super().__init__() is the constructor of the parent class.
+        # It has the node name as an input argument.
+        # Node name: "first_node". This is used to run the node.
+        # Note that the node name is different from the file name.
+
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
+
+
+def main(args=None):  # To install with ROS2.
+    rclpy.init(args=args) # Initialize ROS2 communications.
+    
+    # Node is defined here.
+    node = MyNode()
+    rclpy.spin(node) # Keep the node alive. 
+
+    rclpy.shutdown() # Shutdown ROS2 communications.
+
+if __name__ == '__main__':  # To execute directly from the terminal.
+    main()
